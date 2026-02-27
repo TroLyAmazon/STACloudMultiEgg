e20effb59ab427a85be8733fc083ea5a7fce588c
TLA <congdongamazon@gmail.com>
2026-02-28 02:43:43 +0700

    Refactor entrypoint script to simplify variable parsing and ensure compatibility with bash execution

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -38,12 +38,11 @@ printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mpython --version\n"
 python --version
 
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
-# variable format of "${VARIABLE}" before evaluating the string and automatically
-# replacing the values.
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+# variable format of "${VARIABLE}" before passing to bash for execution.
+# Note: bash itself will expand ${VARIABLE} references when it runs the command.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
-# shellcheck disable=SC2086
 exec /bin/bash -c "${PARSED}"
